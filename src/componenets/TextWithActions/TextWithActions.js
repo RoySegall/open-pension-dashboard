a02da3d859de8279fca4fa91e338369a209b7c5e
RoySegall <roy@segall.io>
2021-03-20 10:59:52 +0200

    More work

--- src/componenets/TextWithActions/TextWithActions.js
+++ src/componenets/TextWithActions/TextWithActions.js
@@ -2,5 +2,20 @@ import "./TextWithActions.scss"
 
 export default ({children, actions}) => <div className="text-with-actions">
   <span>{children}</span>
-  actions
+  <div className="actions-wrapper">
+    <span className="title">Actions</span>
+
+    <div className="menu">
+      <ul>
+        {actions.map((action, key) => {
+          const {title, handler} = action;
+          return <li key={key}><a href={"#"} onClick={(e) => {
+            e.preventDefault();
+            handler();
+          }}>{title}</a> </li>
+        })}
+      </ul>
+    </div>
+
+  </div>
 </div>
